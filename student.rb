de438bd129ac890d13e9ecc28e2c8d567c5261f2
Fezzopro <fezzopro@gmail.com>
2023-07-27 21:30:25 +0200

    Add Student class in a separate file

---
+++ student.rb
@@ -0,0 +1,11 @@
+class Student < Person
+  attr_accessor :classroom
+  def initialize(name, age, parent_permission, classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
