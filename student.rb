e3c7155ffb69e16c4479562fe42609529e846d53
Fezzopro <fezzopro@gmail.com>
2023-07-29 12:36:29 +0200

    Create relationship between Person and Book using the intermediate class Rental

--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(name, age, parent_permission, classroom)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+    classroom.students << self
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.includes?(self)
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    '¯(ツ)/¯'
   end
 end
